284e0afa3a1397dd518954c78df722921d0d8fd1
Francisco <iscojavier24@icloud.com>
2025-12-09 17:08:22 -0300

    feat: implement loan management features including overdue handling and update login to use RUT

--- backend/inventario/admin.py
+++ backend/inventario/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Libro
+from .models import Libro, Ejemplar
 
 # Register your models here.
 
 admin.site.register(Libro)
+admin.site.register(Ejemplar)
+
--- backend/ops/views.py
+++ backend/ops/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from .models import Prestamo, Multa
+from .serializers import PrestamoReadSerializer, PrestamoWriteSerializer, MultaSerializer
+from rest_framework import viewsets
+from django.db import transaction
 
 # Create your views here.
+
+class PrestamoViewSet(viewsets.ModelViewSet):
+    queryset = Prestamo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return PrestamoReadSerializer
+        return PrestamoWriteSerializer
+    @action(detail=True, methods=['post'], url_path='devolver-prestamo')
+    def devolverPrestamo(self, request, pk=None):
+        with transaction.atomic():
+            prestamo = self.get_object()
+
+            if prestamo.estado == 'DEVUELTO':
+                return Response({'detail': 'El préstamo ya ha sido devuelto.'}, status=status.HTTP_400_BAD_REQUEST)
+            devolucion = timezone.now()
+            devolucion_date = devolucion.date()
+            fecha_devolucion_date = prestamo.fecha_devolucion.date()
+            diferencia = (devolucion_date - fecha_devolucion_date).days
+
+            mensaje = 'Préstamo devuelto a tiempo.'
+            datosMulta = None
+
+            if diferencia > 0:
+                valorDia = 1000
+                totalMulta = diferencia * valorDia
+
+                multa = Multa.objects.create(
+                    idPrestamo=prestamo,
+                    diasRetraso=diferencia,
+                    monto=totalMulta,
+                    estadoPago='pendiente',
+                )
+                mensaje = f"Libro devuelto con RETRASO. Se generó una multa de ${totalMulta}."
+                datosMulta = {'id': multa.idMulta, 'monto': multa.monto}
+            
+            prestamo.estado = 'DEVUELTO'
+            prestamo.fecha_devolucion_real = devolucion 
+            prestamo.save()
+
+            return Response({
+                'status': 'ok',
+                'mensaje': mensaje,
+                'multa': datosMulta
+            })
