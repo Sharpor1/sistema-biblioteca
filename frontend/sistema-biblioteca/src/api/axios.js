cc59f9f33e9a32e69db1a3e52df42a0157381ee4
Francisco <iscojavier24@icloud.com>
2025-12-11 19:04:30 -0300

    Please provide the file changes (diffs) to generate a commit message.

--- frontend/sistema-biblioteca/src/api/axios.js
+++ frontend/sistema-biblioteca/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://rinconcitomagico-d2ejfmc8aebdbqag.canadacentral-01.azurewebsites.net/api',
 });
 
 // INTERCEPTOR DE PETICIONES (REQUEST)
